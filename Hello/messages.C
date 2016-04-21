9fd1f81a5ed2e31b1aa9a963fae8af61dccc5321
Chris Karcher <cmkarcher1@gmail.com>
2016-04-20 20:13:36 -0400

    expanded for new messages

---
+++ Hello/messages.C
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "messages.h"
+
+int main() {
+  cout << MESSAGE << endl;
+}
---
+++ Hello/messages.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const string MESSAGE = "Reading this from a header file.";
